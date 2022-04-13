fa991f1cac8703728e7a05117a7f289b1c81be8e
Gabriel <gabrieltsf10@gmail.com>
2022-04-12 21:25:46 -0300

    Adicionando métodos check

--- sistemaBancario/Conta.java
+++ sistemaBancario/Conta.java
@@ -5,8 +5,16 @@ public class Conta {
 	String titular;
 	int numeroDaConta;	
 	double saldo;
+	
+	public void checkTransferencia(Conta destino, double valor) {
+		if(!transferir(Conta destino, double valor)) {
+			System.out.println("Saldo atual: "+this.saldo);
+		}else {
+			System.out.println("Saldo novo: "+this.saldo);
+		}
+	}
 
-	public boolean sacar(double valor) {
+	public double sacar(double valor) {
 		if (this.saldo < valor) {
 			return false;
 		} 
@@ -19,17 +27,20 @@ public class Conta {
 	}
 
 	public boolean transferir(Conta destino, double valor) {
-		this.saldo = this.saldo - valor;
-		destino.saldo = destino.saldo + valor;
+		
 
 		boolean retirou = this.sacar(valor);
 		if (retirou == false) {
+			System.out.println("Saldo insuficiente. Transação Abortada. Faça um depósito antes de transferir.");
 			return false;
 		}
 		else {
+			this.saldo = this.saldo - valor;
+			destino.saldo = destino.saldo + valor;
 			destino.deposita(valor);
+			System.out.println("Transação realizada com sucesso, a conta depósito receberá o valor em até 2 anos úteis");
 			return true;
-
+			
 		}
 	}
 }
